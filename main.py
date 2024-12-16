4bcc3576c8bd2a83aca873dd9147a4e5b403dbe1
Bezalel <bs0583251661@gmail.com>
2024-12-16 09:12:41 +0200

    סידור וניקוי הקוד

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ logger = logging.getLogger(__name__)
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logging.getLogger("telegram").setLevel(logging.WARNING)
 
+BOT_TOKEN = '7349809392:AAHRKfATE1rMImHVejkOeF1Y9afAZz4HE6w'
+
 def main() -> None:
     """Run the bot."""
     setup_database()
     print("Bot is running")
-    application = Application.builder().token('7349809392:AAHRKfATE1rMImHVejkOeF1Y9afAZz4HE6w').build()
+    application = Application.builder().token(BOT_TOKEN).build()
     register_handlers(application)
     try:
         application.run_polling(allowed_updates=Update.ALL_TYPES)
